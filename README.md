ade728a130e72bf86e357992c76063f1ace9b57d
oposit <bgroth@gmail.com>
2023-07-07 17:09:19 +0200

    renaming after copying

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # foilmeet
 My django site for foilmeet.com
+
+
+#reminders for me
+venv: source ~/eb-virt/bin/activate
+
--- foilmeet/foilmeet/asgi.py
+++ foilmeet/foilmeet/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_proj1 project.
+ASGI config for FoilMeet project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_proj1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foilmeet.settings')
 
 application = get_asgi_application()
