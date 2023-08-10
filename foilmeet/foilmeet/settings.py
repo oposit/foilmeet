001201a8535ec0323d15ecac5bfcd34842296104
oposit <bgroth@gmail.com>
2023-08-10 23:49:59 +0200

    Update settings.py

--- foilmeet/foilmeet/settings.py
+++ foilmeet/foilmeet/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^yhp^nifkdt&9m%i767)oqo6a^x!=&l3e*$6=t#@k&averauc='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['54.146.177.240','foilmeet.com','www.foilmeet.com']
 
